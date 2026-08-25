b5f8c99a94883a4a453f8db69f0153e951209008
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-08-25 23:56:26 +0530

    Update round-robin tests for backends

--- internal/loadbalancer/roundrobin_test.go
+++ internal/loadbalancer/roundrobin_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TestRoundRobinNext(t *testing.T) {
-	//parsing 3 backend urls
+	// Parsing 3 backend URLs
 	u1, _ := url.Parse("http://localhost:9000")
 	u2, _ := url.Parse("http://localhost:9001")
 	u3, _ := url.Parse("http://localhost:9002")
 
-	//creating roundrobins with them
+	// Creating round robin with them
 	backends := []*url.URL{u1, u2, u3}
 	rr := NewRoundRobin(backends)
 
@@ -24,27 +24,46 @@ func TestRoundRobinNext(t *testing.T) {
 	}
 
 	for i, exp := range expected {
-		got := rr.Next().String()
+		backend := rr.Next()
+
+		if backend == nil {
+			t.Fatalf("Call %d: expected backend, got nil", i+1)
+		}
+
+		got := backend.URL.String()
+
 		if got != exp {
-			t.Fatalf("Call %d: expected %s, but got %s", i+1, exp, got)
+			t.Fatalf(
+				"Call %d: expected %s, but got %s",
+				i+1,
+				exp,
+				got,
+			)
 		}
 	}
-
 }
 
-func TestRoundRobin_Concurrent(t *testing.T) {
+func TestRoundRobinConcurrent(t *testing.T) {
 	u1, _ := url.Parse("http://b1")
+
 	rr := NewRoundRobin([]*url.URL{u1})
 
 	const workers = 1000
+
 	var wg sync.WaitGroup
 	wg.Add(workers)
 
 	for i := 0; i < workers; i++ {
 		go func() {
 			defer wg.Done()
-			rr.Next() // Without a mutex, this will likely cause a race on rr.next
+
+			backend := rr.Next()
+
+			if backend == nil {
+				t.Errorf("expected backend, got nil")
+			}
 		}()
 	}
+
 	wg.Wait()
 }

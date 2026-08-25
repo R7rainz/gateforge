f88def4c30de531f59e8f836afc992a58c110063
Ronak Kamboj <ronakkamboj26@gmail.com>
2026-08-25 23:56:21 +0530

    Track backend health in load balancer

--- internal/loadbalancer/roundrobin.go
+++ internal/loadbalancer/roundrobin.go
@@ -5,24 +5,63 @@ import (
 	"sync"
 )
 
+type Backend struct {
+	URL     *url.URL
+	Healthy bool
+}
+
 type RoundRobin struct {
-	backends []*url.URL
+	backends []*Backend
 	next     int
 	mu       sync.Mutex
 }
 
 func NewRoundRobin(urls []*url.URL) *RoundRobin {
+	backends := make([]*Backend, 0, len(urls))
+
+	for _, url := range urls {
+		backends = append(backends, &Backend{
+			URL:     url,
+			Healthy: true,
+		})
+	}
+
 	return &RoundRobin{
-		backends: urls,
+		backends: backends,
 	}
 }
 
-func (rr *RoundRobin) Next() *url.URL {
+func (rr *RoundRobin) Next() *Backend {
 	rr.mu.Lock()
 	defer rr.mu.Unlock()
 
-	backend := rr.backends[rr.next]
+	for i := 0; i < len(rr.backends); i++ {
 
-	rr.next = (rr.next + 1) % len(rr.backends)
-	return backend
+		backend := rr.backends[rr.next]
+		rr.next = (rr.next + 1) % len(rr.backends)
+
+		if backend.Healthy {
+			return backend
+		}
+	}
+	return nil
+}
+
+func (rr *RoundRobin) SetHealth(url *url.URL, healthy bool) {
+	rr.mu.Lock()
+	defer rr.mu.Unlock()
+
+	for _, backend := range rr.backends {
+		if backend.URL.String() == url.String() {
+			backend.Healthy = healthy
+			return
+		}
+	}
+}
+
+func (rr *RoundRobin) Backends() []*Backend {
+	rr.mu.Lock()
+	defer rr.mu.Unlock()
+
+	return rr.backends
 }
